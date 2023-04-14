f0594cec85abe6e1a3f107884f6c1f8e67c5358f
BITMMB <antonrusakov@mail.ru>
2023-04-14 14:43:31 +0300

    1

--- src/Components/ResultGrid/ResultGrid.jsx
+++ src/Components/ResultGrid/ResultGrid.jsx
@@ -5,17 +5,11 @@ import { useSelector } from "react-redux";
 
 function ResultGrid() {
   const data = useSelector((state) => state.itemslice.data);
-  const isStarted = useSelector((state) => state.itemslice.isStarted);
+
   let res = data.map((el) => {
     return <Item url={el.urls.small} key={Math.random()} />;
   });
-  return (
-    <div className={classes.grid}>
-      {!isStarted || res.length > 0
-        ? res
-        : "К сожалению, поиск не дал результатов"}
-    </div>
-  );
+  return <div className={classes.grid}>{res}</div>;
 }
 
 export default ResultGrid;
